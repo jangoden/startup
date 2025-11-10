2c730735fb71190835d0007b2d78ae28b2408e88
denirahman <jangoden01@gmail.com>
2025-11-10 16:42:16 +0700

    image priview

--- lib/config.ts
+++ lib/config.ts
@@ -3,5 +3,6 @@ export const siteConfig = {
   siteUrl: 'https://webkeun.web.id',
   siteDescription: 'webkeun - Layanan IT Profesional, Solusi IT untuk bisnis Anda',
   apiUrl: process.env.NEXT_PUBLIC_API_BASE_URL || '/api',
+  defaultOgImage: "https://webkeun.web.id/images/placeholder-template.png",
 };
 
